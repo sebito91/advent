ceb0404b1725bc81435a28d71074316f84a9a306
Sebastian Borza <sebito91@gmail.com>
2017-12-01 23:03:03 -0600

    initial commit, day one

---
+++ 2017/one/one.go
@@ -0,0 +1,11 @@
+package one
+
+// http://adventofcode.com/2017/day/1
+// 1122 produces a sum of 3 (1 + 2) because the first digit (1) matches the second digit and the third digit (2) matches the fourth digit.
+// 1111 produces 4 because each digit (all 1) matches the next.
+// 1234 produces 0 because no digit matches the next.
+// 91212129 produces 9 because the only digit that matches the next one is the last digit, 9.
+
+func DayOne() error {
+	return nil
+}
